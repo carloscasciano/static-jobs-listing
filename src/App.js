b71085ecc8688643e626ae2340ad59b2b75b6f6c
carloscasciano <carlos.casciano@gmail.com>
2020-07-07 11:20:26 -0300

    Add destructuring to Jobpost component, relieved App.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import GlobalStyle from "./styles/global";
 import JobPost from "./components/jobPost/JobPost";
 import Header from "./components/header/Header";
-import Tablet from "./components/tablet/Tablet";
+
 import { dummyJobs } from "./dummyData/dummyJobs";
 console.log(dummyJobs);
 
@@ -28,43 +28,17 @@ function App() {
     <>
       <GlobalStyle />
       <Header />
-      <JobPost />
+      <JobPost 
+        dummyJobs={dummyJobs}
+        filteredLevels={filteredLevels}
+        filteredRoles={filteredRoles}
+        filteredLanguages={filteredLanguages}
+        handleFilteredItems={handleFilteredItems}
+      />
 
-      {filteredLevels.map((i) => (
-        <p key={Math.random()}>{i}</p>
-      ))}
-      {filteredRoles.map((i) => (
-        <p key={Math.random()}>{i}</p>
-      ))}
-      {filteredLanguages.map((i) => (
-        <p key={Math.random()}>{i}</p>
-      ))}
+      
 
-      {dummyJobs.map((j) => (
-        <div key={Math.random()}>
-          <p>{j.company}</p>
-          <p>{j.position}</p>
-          <Tablet
-            handleFilteredItems={handleFilteredItems}
-            name={j.level}
-            section={filteredLevels}
-          ></Tablet>
-          <Tablet
-            handleFilteredItems={handleFilteredItems}
-            name={j.role}
-            section={filteredRoles}
-          ></Tablet>
-          {j.languages.map((l) => (
-            <div key={Math.random()}>
-              <Tablet
-                handleFilteredItems={handleFilteredItems}
-                name={l}
-                section={filteredLanguages}
-              ></Tablet>
-            </div>
-          ))}
-        </div>
-      ))}
+      
     </>
   );
 }
--- src/components/jobPost/JobPost.js
+++ src/components/jobPost/JobPost.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import Tablet from "../tablet/Tablet";
 
-export default function JobPost(props) {
-    return (
-        <div>
-            <p>Job Post</p>
+export default function JobPost({
+  dummyJobs,
+  filteredLevels,
+  filteredRoles,
+  filteredLanguages,
+  handleFilteredItems,
+}) {
+  console.log(dummyJobs);
+  return (
+    <div>
+      <p>Job Post</p>
+      {filteredLevels.map((i) => (
+        <p key={Math.random()}>{i}</p>
+      ))}
+      {filteredRoles.map((i) => (
+        <p key={Math.random()}>{i}</p>
+      ))}
+      {filteredLanguages.map((i) => (
+        <p key={Math.random()}>{i}</p>
+      ))}
+
+      {dummyJobs.map((j) => (
+        <div key={Math.random()}>
+          <p>{j.company}</p>
+          <p>{j.position}</p>
+          <Tablet
+            handleFilteredItems={handleFilteredItems}
+            name={j.level}
+            section={filteredLevels}
+          ></Tablet>
+          <Tablet
+            handleFilteredItems={handleFilteredItems}
+            name={j.role}
+            section={filteredRoles}
+          ></Tablet>
+          {j.languages.map((l) => (
+            <div key={Math.random()}>
+              <Tablet
+                handleFilteredItems={handleFilteredItems}
+                name={l}
+                section={filteredLanguages}
+              ></Tablet>
+            </div>
+          ))}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
